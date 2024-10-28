afc2c1c21563bbcd55c9596e1cb2c29fa6d6b292
TaysaAbinader <139161332+TaysaAbinader@users.noreply.github.com>
2024-10-28 09:02:05 +0200

    Commits

--- Module10/Elevator.py
+++ Module10/Elevator.py
@@ -1,30 +1,31 @@
 class Elevator:
-    def __init__(self):
-        self.bottom_floor = 1
-        self.top_floor = 10
+    def __init__(self, bottom_floor, top_floor):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.current_floor = self.bottom_floor
 
     def go_to_floor(self, floor):
-        self.floor = floor
-        if self.floor > self.bottom_floor:
-            self.floor_up()
-            self.floor = self.
-        elif self.floor < self.top_floor:
-            self.floor_down()
+        while floor != self.current_floor:
+            if self.current_floor < floor:
+                self.floor_up()
+            else:
+                self.floor_down()
 
     def floor_up(self):
-        if self.floor > self.bottom_floor:
-            for i in range(self.floor):
-                print(f"Floor {self.bottom_floor + i}")
-                self.floor = self.top_floor
+        if self.current_floor + 1 <= self.top_floor:
+            previous_floor = self.current_floor
+            self.current_floor += 1
+            print(f"{previous_floor} > floor_up > {self.current_floor}")
 
     def floor_down(self):
-        if self.floor < self.top_floor:
-            for i in range(self.floor):
-                print(f"Floor {self.top_floor - i}")
-                self.floor = self.bottom_floor
+        if self.current_floor -1 >= self.bottom_floor:
+            previous_floor = self.current_floor
+            self.current_floor -= 1
+            print(f"{previous_floor} > floor_down > {self.current_floor}")
 
-h = Elevator()
-floor = int(input("What floor you want to go? "))
-h.go_to_floor(floor)
+
+h = Elevator(1, 10)
+h.go_to_floor(5)
+h.go_to_floor(1)
 
 
